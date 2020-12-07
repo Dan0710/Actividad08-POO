94cb9af56d375500bd51bec88d1fd7641df260ca
Daniel G <daniel70010@gmail.com>
2020-12-06 20:13:30 -0600

    update

--- Actividad08/src/main/java/control/Director.java
+++ Actividad08/src/main/java/control/Director.java
@@ -5,10 +5,43 @@
  */
 package control;
 
+import java.util.ArrayList;
+import modelo.Instrumento;
+
 /**
  *
  * @author Dan
  */
 public class Director {
+    private String nombre;
+    private ArrayList<Instrumento> orquesta;
+
+    public Director(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setOrquesta(ArrayList<Instrumento> orquesta) {
+        this.orquesta = orquesta;
+    }
+
+    public ArrayList<Instrumento> getOrquesta() {
+        return orquesta;
+    }
+    public void crearOrquesta(int numeroInstrumentos){
+        
+    }
+    
+    
+    public void ejecutarPiezaMusical(){
+
+    };
     
 }
