d0a01afa92bc93d9ef3d040996abd531fc079702
elia5 <eliasc20022002@gmail.com>
2020-12-06 20:24:55 -0600

    Se implementan los intrumentos

--- Actividad08/src/main/java/modelo/Instrumento.java
+++ Actividad08/src/main/java/modelo/Instrumento.java
@@ -9,6 +9,55 @@ package modelo;
  *
  * @author Dan
  */
-public class Instrumento {
+public abstract class Instrumento {
+    protected String nombre;
+    protected String nota;
+    protected boolean desperfecto;
+
+    public Instrumento(String nombre, String nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+        desperfecto = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public boolean isDesperfecto() {
+        return desperfecto;
+    }
+
+    public void setDesperfecto(boolean desperfecto) {
+        this.desperfecto = desperfecto;
+    }
+    
+    public String afinarInstrumento() {
+        return "Se afinó el instrumento " + nombre;
+    }
+    
+    public String tocarNota() {
+        return nombre + " toca la nota " + nota;
+    }
+    public String ejecutarPiezaMusical() {
+        return nombre + " está ejecutando su pieza musical";
+    }
+    
+    public abstract void notificarDesperfecto();
+    public abstract void arreglarDesperfecto();
+    
+    
     
 }
---
+++ Actividad08/src/main/java/modelo/Percusion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author elias
+ */
+public class Percusion extends Instrumento {
+    
+    public Percusion() {
+        super("Percusión", "Do");
+    }
+
+    @Override
+    public void notificarDesperfecto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void arreglarDesperfecto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
